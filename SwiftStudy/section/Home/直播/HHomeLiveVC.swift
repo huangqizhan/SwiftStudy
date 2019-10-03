c3bbc14696fa708bcf28f85ed48d8b07c8989159
黄麒展 <757518403@qq.com>
2019-10-03 22:45:36 +0800

    首页优化

--- SwiftStudy/section/Home/直播/HHomeLiveVC.swift
+++ SwiftStudy/section/Home/直播/HHomeLiveVC.swift
@@ -12,8 +12,7 @@ class HHomeLiveVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.white
     }
     
 
