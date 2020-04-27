4c9384816f383cc885a5d6e76be2ebed9f77da1a
huangqizhan <757618403@qq.com>
2020-04-27 18:49:21 +0800

    首页分类

--- SwiftStudy/section/Home/分类/SubClasscify/Api/HClassifySubMenuProvider.swift
+++ SwiftStudy/section/Home/分类/SubClasscify/Api/HClassifySubMenuProvider.swift
@@ -7,7 +7,78 @@
 //
 
 import UIKit
+import Moya
 
-class HClassifySubMenuProvider: NSObject {
+//let HClassifySubMenuProvider = Moya<>
 
+
+
+
+public enum HClassifySubMenuAPI{
+    case headerCategoryList(categoryId:Int)
+    case classifyRecommandList(categoryId:Int)
+    case classifyOtherContentList(keywordId : Int , category: Int)
+}
+
+extension HClassifySubMenuAPI : TargetType{
+    public var baseURL: URL {
+        return URL(string: "http://mobile.ximalaya.com")!
+    }
+    
+    public var path: String {
+        switch self {
+        case .headerCategoryList:
+            return "/discovery-category/keyword/all/1534468874767"
+        case .classifyRecommandList:
+            return "/discovery-category/v2/category/recommend/ts-1534469064622"
+        case .classifyOtherContentList:
+            return "/mobile/discovery/v2/category/metadata/albums/ts-1534469378814"
+        }
+    }
+    
+    public var method: Moya.Method {
+        return Moya.Method.get
+    }
+    
+    public var sampleData: Data {
+        return "".data(using: String.Encoding.utf8)!
+    }
+    
+    public var task: Task {
+        var parmaters = [String : Any]()
+        switch self {
+        case .headerCategoryList(let categoryId):
+            parmaters = ["device":"iPhone","gender":9]
+            parmaters["categoryId"] = categoryId
+        case .classifyRecommandList(let categoryId):
+            parmaters = ["appid":0,
+                         "device":"iPhone",
+                         "gender":9,
+                         "inreview":false,
+                         "network":"WIFI",
+                         "operator":3,
+                         "scale":3,
+                         "uid":124057809,
+                         "version":"6.5.3",
+                         "xt": Int32(Date().timeIntervalSince1970),
+                         "deviceId": UIDevice.current.identifierForVendor!.uuidString]
+            parmaters["categoryId"] = categoryId
+        case .classifyOtherContentList(let keywordId, let category):
+            parmaters = ["calcDimension":"hot",
+                                   "device":"iPhone",
+                                   "pageId":1,
+                                   "pageSize":20,
+                                   "status":0,
+                                   "version":"6.5.3"]
+            parmaters["keywordId"] = keywordId
+            parmaters["categoryId"] = category;
+        }
+        return Task.requestParameters(parameters: parmaters, encoding: URLEncoding.default)
+    }
+    
+    public var headers: [String : String]? {
+        return nil
+    }
+    
+    
 }
--- SwiftStudy/section/Home/分类/SubClasscify/ViewModel/HClassifySubRecommandViewModel.swift
+++ SwiftStudy/section/Home/分类/SubClasscify/ViewModel/HClassifySubRecommandViewModel.swift
@@ -8,6 +8,38 @@
 
 import UIKit
 
-class HClassifySubRecommandViewModel: NSObject {
+/// 添加数据
+typealias HAddDateBlock = ()->Void
 
+class HClassifySubRecommandViewModel : NSObject {
+    
+    var categoryId : Int = 0
+    
+    convenience init(category : Int = 0 ) {
+        self.init()
+        self.categoryId = category
+    }
+    
+    var classifyCategroyContentlist : [HClassifyCategoryContentsList]?
+    var classifyModuleType14List:[HClassifyModuleType14Model]?
+    var classifyModuleType19List:[HClassifyModuleType19Model]?
+    var classifyModuleType20Model:[HClassifyModuleType20Model]?
+    var classifyVerticalModel:[HClassifyVerticalModel]?
+    var focus:HFocusModel?
+    
+    
+    var updateBlock : HAddDateBlock?
+    
 }
+
+/// 请求数据
+extension HClassifySubRecommandViewModel{
+    
+}
+
+/// collectionVuew data 
+extension HClassifySubRecommandViewModel{
+    
+}
+
+
